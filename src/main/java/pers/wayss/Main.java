fda74699f49eaccbd4b231d7ae4579c0d9e221ec
Wayss <wayss000@gmail.com>
2019-10-17 13:08:22 +0800

    增加测试类

---
+++ src/main/java/pers/wayss/Main.java
@@ -0,0 +1,12 @@
+package pers.wayss;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("practice leetcode.");
+    }
+}
+
