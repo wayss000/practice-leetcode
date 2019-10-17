a1e94f2405d1324f477837a86e3911fa05f633ee
Wayss <wayss000@gmail.com>
2019-10-17 13:10:06 +0800

    完成111题

---
+++ src/main/java/pers/wayss/t111/TreeNode.java
@@ -0,0 +1,15 @@
+package pers.wayss.t111;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
