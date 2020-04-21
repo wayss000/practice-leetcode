01800f8b6e1505594cd231421a1ed27deda0263b
wayss <wayss000@gmail.com>
2020-04-21 23:22:59 +0800

    完成第206题

---
+++ src/main/java/pers/wayss/t206/ListNode.java
@@ -0,0 +1,14 @@
+package pers.wayss.t206;
+
+/**
+ * @author Wayss.
+ * @date 2020/4/16.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/pers/wayss/t206/Main.java
@@ -0,0 +1,26 @@
+package pers.wayss.t206;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
