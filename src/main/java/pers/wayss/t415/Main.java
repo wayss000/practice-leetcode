1fb503efe847641ded3a5c0f50fca1d56647740d
Wayss <wayss000@gmail.com>
2020-04-23 21:11:15 +0800

    在做第415题

---
+++ src/main/java/pers/wayss/t415/Main.java
@@ -0,0 +1,17 @@
+package pers.wayss.t415;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        return null;
+    }
+}
