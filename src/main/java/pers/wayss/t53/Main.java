2d4a6b4d3ed6cd169ed0fd219c3074ae20378b84
wayss <wayss000@gmail.com>
2020-05-03 23:04:59 +0800

    第53题

---
+++ src/main/java/pers/wayss/t53/Main.java
@@ -0,0 +1,28 @@
+package pers.wayss.t53;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int curMax = 0;
+
+        for (int num : nums) {
+            curMax = Math.max(curMax + num, num);
+            max = Math.max(curMax, max);
+        }
+        return max;
+    }
+}
