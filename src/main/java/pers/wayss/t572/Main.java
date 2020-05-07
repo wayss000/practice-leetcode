9442686272cd18c9781b035df7bcbae5c8c6fb23
wayss <wayss000@gmail.com>
2020-05-07 23:51:20 +0800

    第572题

---
+++ src/main/java/pers/wayss/t572/Main.java
@@ -0,0 +1,65 @@
+package pers.wayss.t572;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("practice leetcode.");
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null){
+            return false;
+        }
+        return check(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    boolean check(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
+            return true;
+        }
+
+        if (a == null || b == null) {
+            return false;
+        }
+
+        if (a.val == b.val) {
+            return check(a.left, b.left) && check(a.right, b.right);
+        }
+        return false;
+    }
+}
+
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
