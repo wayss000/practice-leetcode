722e4e735aa5f3918db25d9f490cd9c164b0ae34
Wayss <wayss000@gmail.com>
2020-05-09 10:06:50 +0800

    第221题

---
+++ src/main/java/pers/wayss/t221/Main.java
@@ -0,0 +1,48 @@
+package pers.wayss.t221;
+
+/**
+ * 221. 最大正方形
+ * <url>https://leetcode-cn.com/problems/maximal-square/</url>
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        int[][] dp = new int[row][column];
+
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == '1') {
+                    //第一行或第一列，最大只能为1
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
+                    }
+                } else {
+                    dp[i][j] = 0;
+                }
+                maxSide = Math.max(maxSide, dp[i][j]);
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+}
