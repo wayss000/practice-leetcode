7b36b0b3669ecc005f79c31e3008384ec22a0a68
wayss <wayss000@gmail.com>
2020-05-13 23:31:38 +0800

    完成t155题

---
+++ src/main/java/pers/wayss/t155/Main.java
@@ -0,0 +1,70 @@
+package pers.wayss.t155;
+
+import java.util.Stack;
+
+/**
+ * 155. 最小栈
+ * <url>https://leetcode-cn.com/problems/min-stack/</url>
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+
+        if (minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            if (x <= minStack.peek()) {
+                minStack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            int x = stack.pop();
+            if (!minStack.isEmpty()) {
+                if (x == minStack.peek()) {
+                    minStack.pop();
+                }
+            }
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
