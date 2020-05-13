5fe0f390d050f2013a888ace0db7318c3c00025a
wayss <wayss000@gmail.com>
2020-05-13 23:57:56 +0800

    完成t102题

---
+++ src/main/java/pers/wayss/t102/Main.java
@@ -0,0 +1,61 @@
+package pers.wayss.t102;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ * <url>https://leetcode-cn.com/problems/binary-tree-level-order-traversal/</url>
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                TreeNode temp = queue.poll();
+                level.add(temp.val);
+                //左子树不为空，队列中添加左
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+                //右子树不为空，队列中添加右
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
