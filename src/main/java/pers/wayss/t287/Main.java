85fdd09edd808a10407475558a9f8ae154853a6d
wayss <wayss000@gmail.com>
2020-05-28 23:49:52 +0800

    t287题

---
+++ src/main/java/pers/wayss/t287/Main.java
@@ -0,0 +1,17 @@
+package pers.wayss.t287;
+
+/**
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        return 0;
+    }
+}
