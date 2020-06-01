ac2073c8f3289af02549429bded601ffa9c505a2
wayss <wayss000@gmail.com>
2020-06-01 23:12:12 +0800

    完成t1431题

---
+++ src/main/java/pers/wayss/t1431/Main.java
@@ -0,0 +1,37 @@
+package pers.wayss.t1431;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 1431. 拥有最多糖果的孩子
+ * https://leetcode-cn.com/problems/kids-with-the-greatest-number-of-candies/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandy = 0;
+        for (int val : candies) {
+            if (val > maxCandy) {
+                maxCandy = val;
+            }
+        }
+        Boolean[] result = new Boolean[candies.length];
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies < maxCandy) {
+                result[i] = false;
+            } else {
+                result[i] = true;
+            }
+        }
+        return Arrays.asList(result);
+    }
+}
