0958034afb04dcf050d2a6a0feb77071bd755c10
wayss <wayss000@gmail.com>
2020-06-02 23:23:57 +0800

    完成t64题

---
+++ src/main/java/pers/wayss/t64/Main.java
@@ -0,0 +1,20 @@
+package pers.wayss.t64;
+
+/**
+ * 64. 求1+2+…+n
+ * https://leetcode-cn.com/problems/qiu-12n-lcof/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int sumNums(int n) {
+        return n == 0 ? 0 : n + sumNums(n - 1);
+    }
+}
