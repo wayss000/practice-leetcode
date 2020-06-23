537deacf86eed7ef28361a1ce94cd91335691923
wayss <wayss000@gmail.com>
2020-06-23 23:29:19 +0800

    第67题

---
+++ src/main/java/pers/wayss/t67/Main.java
@@ -0,0 +1,27 @@
+package pers.wayss.t67;
+
+/**
+ * 67. 二进制求和
+ * https://leetcode-cn.com/problems/add-binary/
+ *
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+        Solution solution = new Solution();
+        //这俩测试用例会报错
+        String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+        String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+        solution.addBinary(a,b);
+    }
+}
+
+class Solution {
+    public String addBinary(String a, String b) {
+        Long result = Long.parseLong(a, 2) + Long.parseLong(b, 2);
+        return Long.toBinaryString(result);
+    }
+}
