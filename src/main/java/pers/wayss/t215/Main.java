6a1da25f8bbe65af23a83a5ff3b8d9be2813c814
Wayss <wayss000@gmail.com>
2020-06-29 21:07:27 +0800

    第215题

---
+++ src/main/java/pers/wayss/t215/Main.java
@@ -0,0 +1,33 @@
+package pers.wayss.t215;
+
+import java.util.PriorityQueue;
+
+/**
+ * 215. 数组中的第K个最大元素
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
+        for (int num : nums){
+            if (queue.size() < k){
+                queue.add(num);
+            }else {
+                if (num > queue.peek()){
+                    queue.poll();
+                    queue.add(num);
+                }
+            }
+        }
+        return queue.peek();
+    }
+}
