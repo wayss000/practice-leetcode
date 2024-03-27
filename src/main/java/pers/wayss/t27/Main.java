53fa858c5cdb3dca9100c981a8b4f654ebda187f
wayss000 <wayss000@gmail.com>
2024-03-27 14:23:32 +0800

    add

---
+++ src/main/java/pers/wayss/t27/Main.java
@@ -0,0 +1,29 @@
+package pers.wayss.t27;
+
+/**
+ * 27. 移除元素
+ * https://leetcode.cn/problems/remove-element/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
