dc70582c48bce0e78d82cf9f3debaac9baaa4f3c
wayss000 <wayss000@gmail.com>
2024-03-27 14:51:45 +0800

    add

---
+++ src/main/java/pers/wayss/t344/Main.java
@@ -0,0 +1,28 @@
+package pers.wayss.t344;
+
+/**
+ * 344. 反转字符串
+ * https://leetcode.cn/problems/reverse-string/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
