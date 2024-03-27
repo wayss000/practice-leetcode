34a923f0f6d67cd2703b47b3c15184c6401bd60e
wayss000 <wayss000@gmail.com>
2024-03-27 11:08:36 +0800

    add

---
+++ src/main/java/pers/wayss/t26/Main.java
@@ -0,0 +1,33 @@
+package pers.wayss.t26;
+
+/**
+ * 26. 删除有序数组中的重复项
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        // 数组长度为索引 + 1
+        return slow + 1;
+    }
+}
