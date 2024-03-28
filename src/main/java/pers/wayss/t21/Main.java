1dbf3d092abc905d6e4a5c598a9757f413d2b8e2
wayss000 <wayss000@gmail.com>
2024-03-28 16:57:33 +0800

    add

---
+++ src/main/java/pers/wayss/t21/Main.java
@@ -0,0 +1,50 @@
+package pers.wayss.t21;
+
+import pers.wayss.ListNode;
+
+/**
+ * 21. 合并两个有序链表
+ * https://leetcode.cn/problems/merge-two-sorted-lists/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode temp = new ListNode(-1);
+        ListNode p = temp;
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            p = p.next;
+        }
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) {
+            p.next = p2;
+        }
+        return temp.next;
+    }
+}
+
