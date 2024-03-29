a49193ee02c2f7d3ceaa38162d0d192a7d53ff12
wayss000 <wayss000@gmail.com>
2024-03-29 17:30:59 +0800

    add

---
+++ src/main/java/pers/wayss/t142/Main.java
@@ -0,0 +1,47 @@
+package pers.wayss.t142;
+
+import pers.wayss.ListNode;
+
+/**
+ * 142. 环形链表 II
+ * https://leetcode.cn/problems/linked-list-cycle-ii/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        // 先判断是否有环
+        if (head == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        // 算法的核心：慢指针的k-m = 快指针的k-m，算法很精妙参考下面：
+        // 参考：https://labuladong.online/algo/essential-technique/linked-list-skills-summary-2/#%E5%88%A4%E6%96%AD%E9%93%BE%E8%A1%A8%E6%98%AF%E5%90%A6%E5%8C%85%E5%90%AB%E7%8E%AF
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
