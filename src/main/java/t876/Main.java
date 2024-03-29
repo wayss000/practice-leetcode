7b4efc5ceaf39587f02154c32667e3b672654886
wayss000 <wayss000@gmail.com>
2024-03-29 16:32:37 +0800

    add

---
+++ src/main/java/t876/Main.java
@@ -0,0 +1,32 @@
+package t876;
+
+import pers.wayss.ListNode;
+
+/**
+ * 876. 链表的中间结点
+ * https://leetcode.cn/problems/middle-of-the-linked-list/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
