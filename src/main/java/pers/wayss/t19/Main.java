08ed7cf91e0ffeaa744b31382ca3a1fcf201befc
wayss000 <wayss000@gmail.com>
2024-03-29 16:07:43 +0800

    add

---
+++ src/main/java/pers/wayss/t19/Main.java
@@ -0,0 +1,45 @@
+package pers.wayss.t19;
+
+import pers.wayss.ListNode;
+
+/**
+ * 19. 删除链表的倒数第 N 个结点
+ * https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+ *
+ * @author Wayss.
+ * @date 2019/10/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dump = new ListNode(-1);
+        dump.next = head;
+        ListNode node = theNode(dump, n + 1);
+        node.next = node.next.next;
+        return dump.next;
+    }
+
+    public ListNode theNode(ListNode head, int n) {
+        ListNode p1 = head;
+        for (int i = 0; i < n; i++) {
+            p1 = p1.next;
+        }
+        ListNode p2 = head;
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2;
+    }
+}
+
