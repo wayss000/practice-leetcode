a1772a7f305d9fabdf128d913009cc1c4ffafc85
wayss <wayss000@gmail.com>
2024-06-25 00:02:27 +0800

    add

--- src/main/java/pers/wayss/t215/Main.java
+++ src/main/java/pers/wayss/t215/Main.java
@@ -1,5 +1,6 @@
 package pers.wayss.t215;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -17,12 +18,17 @@ public class Main {
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
-        for (int num : nums){
-            if (queue.size() < k){
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        for (int num : nums) {
+            if (queue.size() < k) {
                 queue.add(num);
-            }else {
-                if (num > queue.peek()){
+            } else {
+                if (num > queue.peek()) {
                     queue.poll();
                     queue.add(num);
                 }
