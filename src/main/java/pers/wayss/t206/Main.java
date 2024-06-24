67bbc502b8d07aaa5bd3149a08e82fd8a2b66985
wayss000 <wayss000@gmail.com>
2024-06-24 21:03:05 +0800

    add

--- src/main/java/pers/wayss/t206/Main.java
+++ src/main/java/pers/wayss/t206/Main.java
@@ -19,9 +19,10 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
+        ListNode next = null;
 
         while (curr != null) {
-            ListNode next = curr.next;
+            next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
