5da283ffeaa0bc8f3cc17ffe3b80b832a3c7fcb9
wayss000 <wayss000@gmail.com>
2024-06-28 13:30:25 +0800

    add

--- src/main/java/pers/wayss/t160/Main.java
+++ src/main/java/pers/wayss/t160/Main.java
@@ -21,7 +21,7 @@ class Solution {
         // 双指针思路 题解：https://labuladong.online/algo/essential-technique/linked-list-skills-summary-2/#%E4%B8%A4%E4%B8%AA%E9%93%BE%E8%A1%A8%E6%98%AF%E5%90%A6%E7%9B%B8%E4%BA%A4
         ListNode h1 = headA;
         ListNode h2 = headB;
-        // 如果两个链表未相交，下面就死循环了
+        // 如果两个链表未相交，下面会死循环吗？答案不会，因为不相交，拼接一次之后，h1会等于h2，都为null的。
         while (h1 != h2) {
             if (h1 != null) {
                 h1 = h1.next;
