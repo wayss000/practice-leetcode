56bbf04bee3d969f3cddcb2f57efe6fae69621b1
wayss000 <wayss000@gmail.com>
2024-07-02 17:36:23 +0800

    add

---
+++ src/main/java/pers/wayss/t704/Main.java
@@ -0,0 +1,41 @@
+package pers.wayss.t704;
+
+/**
+ * 704. 二分查找
+ * https://leetcode.cn/problems/binary-search/description/
+ *
+ * 数据结构：数组
+ * 算法：二分查找
+ *
+ * @author Wayss.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+        Solution solution = new Solution();
+        int[] nums = {5};
+        int target = 5;
+        int search = solution.search(nums, target);
+        System.out.println(search);
+    }
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right--;
+            }
+            if (nums[mid] < target) {
+                left++;
+            }
+        }
+        return -1;
+    }
+}
