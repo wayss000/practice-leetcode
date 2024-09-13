d70c958d27b666a080b653481f7f495cd02ad7ac
wayss <wayss000@gmail.com>
2024-09-13 23:51:39 +0800

    add

---
+++ src/main/java/pers/wayss/t122/Main.java
@@ -0,0 +1,28 @@
+package pers.wayss.t122;
+
+/**
+ * 122. 买卖股票的最佳时机 II
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
+ * <p>
+ * 数据结构：数组
+ * 算法：贪心
+ *
+ * @author Wayss.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int tmp = prices[i] - prices[i - 1];
+            if (tmp > 0) profit += tmp;
+        }
+        return profit;
+
+    }
+}
