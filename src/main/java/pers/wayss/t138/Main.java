2d44824e3e666966ddec96337640d296d7fdbe18
wayss <wayss000@gmail.com>
2024-09-18 23:56:01 +0800

    add

---
+++ src/main/java/pers/wayss/t138/Main.java
@@ -0,0 +1,53 @@
+package pers.wayss.t138;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 138. 随机链表的复制
+ * https://leetcode.cn/problems/copy-list-with-random-pointer/description/
+ * <p>
+ * 数据结构：链表、哈希表
+ * 算法：遍历链表
+ *
+ * @author Wayss.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("practice leetcode.");
+    }
+}
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        // 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        // 4. 构建新链表的 next 和 random 指向
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        // 5. 返回新链表的头节点
+        return map.get(head);
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
