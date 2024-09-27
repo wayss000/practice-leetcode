9eaa6149910648229ed3fa665046d2b85122e7ac
wayss <wayss000@gmail.com>
2024-09-27 13:26:08 +0800

    add

--- src/main/java/pers/wayss/t704/Main.java
+++ src/main/java/pers/wayss/t704/Main.java
@@ -25,7 +25,7 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = (right + left) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
